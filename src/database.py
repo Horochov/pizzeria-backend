6034f5ae589299eba9a3069e51f2eca3900bf20a
FilipJQ77 <filipprzygonski99@gmail.com>
2020-12-21 20:06:52 +0100

    minor improvements overall

--- src/database.py
+++ src/database.py
@@ -3,21 +3,31 @@ import psycopg2
 import simplejson
 
 
+def connection_start():
+    connection = psycopg2.connect(dbname="postgres", user="postgres", password="postgres")
+    cursor = connection.cursor()
+    return connection, cursor
+
+
+def connection_end(connection, cursor):
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
 def main():
     app = flask.Flask("")
 
     @app.route('/menu')
-    def select():
-        connection = psycopg2.connect(dbname="restaurant", user="postgres", password="postgres")
-        cursor = connection.cursor()
+    def select_menu():
+        connection, cursor = connection_start()
 
         cursor.execute("""SELECT * FROM restaurant_schema.products""")
         products = cursor.fetchall()
         json_products = simplejson.dumps(products)
 
-        connection.commit()
-        cursor.close()
-        connection.close()
+        connection_end(connection, cursor)
+
         return json_products
 
     app.run()
