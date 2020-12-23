e2a1fa3dffe34a669dcb175ac83662b5e769abd0
FilipJQ77 <filipprzygonski99@gmail.com>
2020-12-23 14:42:09 +0100

    /addorder added but doesn't work, divided database and logic into separate beings

--- src/database.py
+++ src/database.py
@@ -1,10 +1,11 @@
+import requests
 import flask
 import psycopg2
 import simplejson
 
 
 def connection_start():
-    connection = psycopg2.connect(dbname="postgres", user="postgres", password="postgres")
+    connection = psycopg2.connect(dbname="restaurant", user="postgres", password="postgres")
     cursor = connection.cursor()
     return connection, cursor
 
@@ -15,20 +16,32 @@ def connection_end(connection, cursor):
     connection.close()
 
 
+def database_get_products():
+    connection, cursor = connection_start()
+
+    cursor.execute("""SELECT * FROM restaurant_schema.products""")
+    products = cursor.fetchall()
+
+    connection_end(connection, cursor)
+
+    return products
+
+
 def main():
     app = flask.Flask("")
 
     @app.route('/menu')
     def select_menu():
-        connection, cursor = connection_start()
-
-        cursor.execute("""SELECT * FROM restaurant_schema.products""")
-        products = cursor.fetchall()
+        products = database_get_products()
         json_products = simplejson.dumps(products)
-
-        connection_end(connection, cursor)
-
         return json_products
+
+    # todo
+    @app.route('/addorder', methods=['GET', 'POST'])
+    def add_order():
+        content = flask.request.json
+        print(content)
+        return content
 
     app.run()
 
