b642a730ba87e154cc2468723b4cf5b786076239
Bartosz Hornicki <horochov@10g.pl>
2021-01-21 19:58:24 +0100

    added unit tests, previous tests were already acceptance tests

--- src/test.py
+++ src/test.py
@@ -2,6 +2,10 @@ import requests
 from backend import *
 from unittest.mock import MagicMock
 import unittest
+import hashlib
+import database
+
+repo = PizzeriaRepository("postgres", "postgres")
 
 def testLogin(user, password):
     headers = {"Accept": "application/json",
@@ -21,12 +25,32 @@ def testLogin(user, password):
 class TestStringMethods(unittest.TestCase):
 
     def test_login_real_user(self):
-        self.assertEqual(testLogin("JS","admin12345"), True)
-
+        user = "JS"
+        password = "admin12345"
+        password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        self.assertEqual(repo.login(user,password), True)
+        
     def test_login_fake_user(self):
-        self.assertEqual(testLogin("AB","admin12345"), False)
+        user = "AB"
+        password = "admin12345"
+        password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        self.assertEqual(repo.login(user,password), False)
+        
         
     def test_login_real_user_wrong_password(self):
+        user = "AB"
+        password = "admin123456"
+        password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        self.assertEqual(repo.login(user,password), False)
+        
+    
+    def test_login_server_real_user(self):
+        self.assertEqual(testLogin("JS","admin12345"), True)
+
+    def test_login_server_fake_user(self):
+        self.assertEqual(testLogin("AB","admin12345"), False)
+        
+    def test_login_server_real_user_wrong_password(self):
         self.assertEqual(testLogin("JS","admin123456"), False)
 
 
